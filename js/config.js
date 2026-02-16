c32a7b4862f0bd0528c35674a4e8a83767f98836
mestevaw <mestevaw@gmail.com>
2026-02-16 09:40:32 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,6 +6,11 @@
 const SUPABASE_URL = 'https://tzuvoceilkqurnujrraq.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6dXZvY2VpbGtxdXJudWpycmFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1Mjg3NDIsImV4cCI6MjA4NTEwNDc0Mn0.2bGNuh7nVhAFGULlXBT85wsKUMgJJbEEGtUq0jerqno';
 
+// Google Drive API
+const GOOGLE_API_KEY = 'AIzaSyDq6f3LIpyFSF3wxG41Q1Cn5_Ia8pbMWT8';
+const GOOGLE_CLIENT_ID = '190984074535-01jfm3brrtn17qqeb258klt1mv0qqi0k.apps.googleusercontent.com';
+const GOOGLE_SCOPES = 'https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.readonly';
+
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // Variables globales de estado
