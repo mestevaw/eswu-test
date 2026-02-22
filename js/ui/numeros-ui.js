905a228ab786e7aeb9304c919c851b55a452550d
mestevaw <mestevaw@gmail.com>
2026-02-21 21:50:13 -0600

    Update numeros-ui.js

--- js/ui/numeros-ui.js
+++ js/ui/numeros-ui.js
@@ -5,6 +5,7 @@
 let currentHomeTable = null;
 
 function showNumerosPage() {
+    if (isMobile()) hideMobileMenu();
     document.getElementById('adminSubMenu').classList.remove('active');
     document.querySelectorAll('.page').forEach(p => p.classList.remove('active'));
     document.getElementById('numerosPage').classList.add('active');
